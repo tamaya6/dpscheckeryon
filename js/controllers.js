78bded741e5496ebb255ddce8c7e7e9dc7677295
tamaya6 <tamaya6@gmail.com>
2013-11-29 15:15:58 +0900

    加算値トータル計算追加

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 var dpscheckeryon = angular.module('dpscheckeryon', {
   setup: function() {
-    // setup for module name
+// setup for module name
   },
   teardown: function() {
     //teardown for module name
@@ -15,5 +15,41 @@ dpscheckeryon.controller('dpscheckeryonCtrl', function ($scope) {
     $scope.Waists = waistData;
     $scope.Legs = legData;
     $scope.Foots = footData;
+
+    $scope.getStrTotal = function (item) {
+      var r = 0;
+      for (var k in item) r += item[k].str;
+      return r;
+    }
+
+    $scope.getVitTotal = function (item) {
+      var r = 0;
+      for (var k in item) r += item[k].vit;
+      return r;
+    }
+
+    $scope.getAccTotal = function (item) {
+      var r = 0;
+      for (var k in item) r += item[k].acc;
+      return r;
+    }
+
+    $scope.getCritTotal = function (item) {
+      var r = 0;
+      for (var k in item) r += item[k].crit;
+      return r;
+    }
+
+    $scope.getDetTotal = function (item) {
+      var r = 0;
+      for (var k in item) r += item[k].det;
+      return r;
+    }
+
+    $scope.getSspTotal = function (item) {
+      var r = 0;
+      for (var k in item) r += item[k].ssp;
+      return r;
+    }
 
 });
