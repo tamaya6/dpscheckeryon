8066c0538ff433dc9055be34db511fd10e6d4083
tamaya6 <tamaya6@gmail.com>
2013-12-02 13:18:06 +0900

    その他計算追加

--- js/controllers.js
+++ js/controllers.js
@@ -9,6 +9,7 @@ var dpscheckeryon = angular.module('dpscheckeryon', {
 
 dpscheckeryon.controller('dpscheckeryonCtrl', function ($scope) {
     $scope.item = [];
+    $scope.baseParameter = [];
     $scope.Weapons = weaponData;
     $scope.Heads = headData;
     $scope.Bodys = bodyData;
@@ -20,6 +21,9 @@ dpscheckeryon.controller('dpscheckeryonCtrl', function ($scope) {
     $scope.Ears = earData;
     $scope.Wrists = wristData;
     $scope.Rings = ringData;
+
+    $scope.wsdmg = 180;
+    $scope.jinrai = 0;
 
     $scope.item.weapon = $scope.Weapons[0];
     $scope.item.head = $scope.Heads[0];
@@ -33,6 +37,13 @@ dpscheckeryon.controller('dpscheckeryonCtrl', function ($scope) {
     $scope.item.wrist = $scope.Wrists[0];
     $scope.item.ring1 = $scope.Rings[0];
     $scope.item.ring2 = $scope.Rings[0];
+
+    $scope.baseParameter.str = 265;
+    $scope.baseParameter.vit = 202;
+    $scope.baseParameter.acc = 341;
+    $scope.baseParameter.crit = 341;
+    $scope.baseParameter.det = 202;
+    $scope.baseParameter.ssp = 341;
 
 
     // アイテム加算分のparameterのトータル
@@ -67,8 +78,12 @@ dpscheckeryon.controller('dpscheckeryonCtrl', function ($scope) {
           r = {},
           at = this.getAllTotal(b,it);
 
+      // AA間隔（※武器固有ではなく迅雷込値）
+      r['aaDelay'] = i.weapon.aadel*(1-($scope.jinrai*0.05));
+
       // AA威力
       r['aaDmg'] = (i.weapon.aadam / i.weapon.pyhd) * 100;
+
       // クリティカル確率
       r['critAve'] = at.crit * 0.0693 - 18.486;
 
@@ -77,6 +92,24 @@ dpscheckeryon.controller('dpscheckeryonCtrl', function ($scope) {
 
       // AA平均ダメ
       r['aaAveDmg'] = (r['aaDmg']/100)*((at.str*0.0032+0.4162)*i.weapon.pyhd+(at.str*0.1001-0.3529)+(at.det-202)*0.11)*r['critMag'];
+
+      // WS平均ダメ
+      r['wsAveDmg'] = ($scope.wsdmg/100)*((at.str*0.0032+0.4162)*i.weapon.pyhd+(at.str*0.1001-0.3529)+(at.det-202)*0.035)*r['critMag'];
+
+      // GCD
+      r['gcd'] = (2.5-(it.ssp/1000))*(1-($scope.jinrai*0.05));
+
+      // AA DPS
+      r['aaDps'] = r['aaAveDmg']/r['aaDelay']*(1+$scope.jinrai*0.07);
+
+      // WS DPS
+      r['wsDps'] = r['wsAveDmg']/r['gcd']*(1+$scope.jinrai*0.07);
+
+      // 総DPS
+      r['allDps'] = r['aaDps']+r['wsDps'];
+
+      // 基礎DPS
+      r['basicDps'] = ((r['aaDmg']/100)*((b.str*0.0032+0.4162)*i.weapon.pyhd+(b.str*0.1001-0.3529)+(b.det-202)*0.11)*(1+0.5*(b.crit*0.0693-18.486)/100)/r['aaDelay'])+(($scope.wsdmg/100)*((b.str*0.0032+0.4162)*i.weapon.pyhd+(b.str*0.1001-0.3529)+(b.det-202)*0.035)*(1+0.5*(b.crit*0.0693-18.486)/100)/(2.5-$scope.jinrai*0.05))
 
       //r['base'] = b;
       //r['item'] = i;
